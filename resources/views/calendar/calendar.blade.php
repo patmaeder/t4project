950bb10a8ba0cbc23b16d1aa0190b1c3725dc462
Patrick Mäder <padi.maeder@web.de>
2020-11-18 12:26:45 +0100

    Redesigned Calendar

--- resources/views/calendar/calendar.blade.php
+++ resources/views/calendar/calendar.blade.php
@@ -13,45 +13,54 @@
         {{session()->get('error')}}
     </div>
     @endif
+    
+    <div class="card">
+        <div class="card-header">
 
-    <div class="d-flex justify-content-between mb-4">
-        <button type="button" class="btn btn-link" id="preceding">
-            <i class="fas fa-chevron-left"></i>
-        </button>
-        <h2>&nbsp;</h2>
-        <button type="button" class="btn btn-link" id="following">
-         <i class="fas fa-chevron-right"></i>
-        </button>
+            <div class="d-flex justify-content-between">
+                <button type="button" class="btn btn-link" id="preceding">
+                    <i class="fas fa-chevron-left"></i>
+                </button>
+                <h2 class="mb-0" style="color: #333">&nbsp;</h2>
+                <button type="button" class="btn btn-link" id="following">
+                <i class="fas fa-chevron-right"></i>
+                </button>
+            </div>
+
+        </div>
+        <div class="card-body">
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><p>Montag</p></th>
+                        <th><p>Dienstag</p></th>
+                        <th><p>Mittwoch</p></th>
+                        <th><p>Donnerstag</p></th>
+                        <th><p>Freitag</p></th>
+                        <th><p>Samstag</p></th>
+                        <th><p>Sonntag</p></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    @for ($i = 1; $i <= 6; $i++)
+                    <tr>
+
+                        @for ($a = 1; $a <= 7; $a++)
+                        <td class="day"><div class="cell_wrapper"><span>&nbsp</span></div></td>
+                        @endfor 
+
+                    </tr>
+                    @endfor
+                    
+                </tbody>
+            </table>
+
+            <a href="{{ route('calendar.create') }}"><button type="button" class="btn btn-primary">Neuer Termin</button></a>
+
+        </div>
     </div>
-
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th><p>Montag</p></th>
-                <th><p>Dienstag</p></th>
-                <th><p>Mittwoch</p></th>
-                <th><p>Donnerstag</p></th>
-                <th><p>Freitag</p></th>
-                <th><p>Samstag</p></th>
-                <th><p>Sonntag</p></th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @for ($i = 1; $i <= 6; $i++)
-            <tr>
-
-                @for ($a = 1; $a <= 7; $a++)
-                <td class="day"><div class="cell_wrapper"><span>&nbsp</span></div></td>
-                @endfor 
-
-            </tr>
-            @endfor
-            
-        </tbody>
-    </table>
-
-    <a href="{{ route('calendar.create') }}"><button type="button" class="btn btn-primary">Neuer Termin</button></a>
 
 </div>
 
